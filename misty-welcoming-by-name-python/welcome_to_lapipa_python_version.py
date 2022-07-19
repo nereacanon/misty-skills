8da9a26f647e1e2b337651abf0d0b6b6e738798c
Manolo Rosario Evans Diaz <dragon.evans@gmail.com>
2022-07-19 15:30:11 +0200

    Welcome to La Pipa.py

---
+++ misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
@@ -0,0 +1,8 @@
+from mistyPy.Robot import Robot
+from mistyPy.Events import Events
+from mistyPy.EventFilters import EventFilters
+
+misty.start_face_recognition()
+
+def registerFaceDetection ():
+    misty.
