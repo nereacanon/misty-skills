02a9c3efb78eb230163909d0b740818a87e954de
Nerea <nerea@bedrockdbd.com>
2022-07-22 10:44:13 +0200

    FaceDetect

--- misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
+++ misty-welcoming-by-name-python/welcome_to_lapipa_python_version.py
@@ -4,34 +4,45 @@ from mistyPy.EventFilters import EventFilters
 import json
 misty=Robot(ip='192.168.128.86')
 
-misty.StartFaceRecognition()
-misty.RegisterEvent("FaceDetect", Events.FaceRecognition, 8000,callback_function=FaceDetect)
 
-def FaceDetect(face):
-    x=json.loads(misty.GetKnownFaces().text)['results']
-    if (face not in x ):
-     #misty.Set("faceInFOV", true, false)
-         misty.ChangeLED(148, 0, 211)
-         misty.DisplayImage("e_Joy2.jpg")
-         misty.Speak("Hello human, welcome to La Pipa! What's your name?")
-         misty.Stop(3500)
+
+
+def start():
+    misty.RegisterEvent("Face", Events.FaceRecognition,callback_function=FaceDetect)
+    misty.StartFaceDetection()
+    misty.StartFaceRecognition()
+
+
+
+    
+def FaceDetect(event):
+    print(event)
+    print(type(event))
+    misty.StopFaceRecognition()
+    misty.StopFaceDetection()
+    Name=event['message']['personName']
+
+    if Name=='unknown person':
+        ##misty.Set("faceInFOV", true, false)
+        misty.ChangeLED(148, 0, 211)
+        misty.DisplayImage("e_Contempt.jpg")
+        misty.Speak("Hello human, welcome to La Pipa! What's your name?")
+        #misty.Stop(3500)
+
+        #misty.RegisterEvent('FaceTraining',Events.FaceTraining,callback_function=FaceTraining)
+
+
     
     else:
         #misty.Set("faceInFOV", true, false);
         misty.ChangeLED(0, 0, 111)
         misty.DisplayImage("e_Joy2.jpg")
-        misty.Speak("Hi ", face ,", we've met before! How are you?")
+        string = 'Hi '+ Name+ ' we have met before'
+        misty.Speak(string)
 
-        misty.MoveArm("both", -26, 100)
-        misty.Stop(1000)
-        misty.MoveArm("both", 90, 100)
-        
-
-
- 
- 
- 
-
+        #misty.MoveArm("both", -26, 100)
+        #misty.Stop(1000)
+        #misty.MoveArm("both", 90, 100)
 
 
 
